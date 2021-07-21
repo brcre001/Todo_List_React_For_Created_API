59e5305f1a8b1a0bced7e7b56f043af763235b5d
brcre001 <brandon.f.creed@gmail.com>
2021-07-21 22:46:00 +0000

    used async functions to handle fetch instead of promises

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -12,44 +12,73 @@ export const TodoList = () => {
 
 	// UseEffect for retrieving stored values from database on mount
 	useEffect(() => {
-		fetch(apiURL)
-			.then(resp => {
-				console.log(resp.status);
-				if (resp.status >= 200 && resp.status < 300) {
-					return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-				} else {
-					alert(
-						`This is a client error, its your fault this is a ${resp.status}`
-					);
-					throw Error(`${resp.ok} ${resp.status}`);
-				}
-				// console.log(resp.ok); // will be true if the response is successfull
-				// console.log(resp.status); // the status code = 200 or code = 400 etc.
-			})
-			.then(data => {
-				//here is were your code should start after the fetch finishes
-				// console.log(data); //this will print on the console the exact object received from the server
-				setListArray(data);
-			})
-			.catch(error => {
-				//error handling
-				console.log("This is an error: ", error);
-			});
+		// fetch(apiURL)
+		// 	.then(resp => {
+		// 		console.log(resp.status);
+		// 		if (resp.status >= 200 && resp.status < 300) {
+		// 			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		// 		} else {
+		// 			alert(
+		// 				`This is a client error, its your fault this is a ${resp.status}`
+		// 			);
+		// 			throw Error(`${resp.ok} ${resp.status}`);
+		// 		}
+		// 		// console.log(resp.ok); // will be true if the response is successfull
+		// 		// console.log(resp.status); // the status code = 200 or code = 400 etc.
+		// 	})
+		// 	.then(data => {
+		// 		//here is were your code should start after the fetch finishes
+		// 		// console.log(data); //this will print on the console the exact object received from the server
+		// 		setListArray(data);
+		// 	})
+		// 	.catch(error => {
+		// 		//error handling
+		// 		console.log("This is an error: ", error);
+		// 	});
+		initList();
 	}, []);
 
 	// UseEffect for updating Todo List array values
 	useEffect(() => {
-		fetch(apiURL, {
+		// fetch(apiURL, {
+		// 	method: "PUT", // or 'POST'
+		// 	body: JSON.stringify(listArray), // data can be `string` or {object}!
+		// 	headers: {
+		// 		"Content-Type": "application/json"
+		// 	}
+		// })
+		// 	.then(res => res.json())
+		// 	.then(response => console.log("Success:", JSON.stringify(response)))
+		// 	.catch(error => console.error("Error:", error));
+		updateList();
+	}, [listArray]);
+
+	/* NEW SET OF FUNCTIONS USING ASYNC */
+	const initList = async () => {
+		const response = await fetch(apiURL);
+		try {
+			const data = await response.json();
+			setListArray(data);
+		} catch (error) {
+			throw new Error(error);
+		}
+	};
+
+	const updateList = async () => {
+		const response = await fetch(apiURL, {
 			method: "PUT", // or 'POST'
 			body: JSON.stringify(listArray), // data can be `string` or {object}!
 			headers: {
 				"Content-Type": "application/json"
 			}
-		})
-			.then(res => res.json())
-			.then(response => console.log("Success:", JSON.stringify(response)))
-			.catch(error => console.error("Error:", error));
-	}, [listArray]);
+		});
+		try {
+			const data = await response.json();
+			console.log("Success:", JSON.stringify(data));
+		} catch (error) {
+			throw new Error(error);
+		}
+	};
 
 	// Function to add Todo List item when enter key is pressed
 	const addItem = event => {
@@ -114,7 +143,7 @@ export const TodoList = () => {
 
 	// This returns the structure of the webpage
 	return (
-		<div className="col-6 mx-auto p-2">
+		<div className="col-8 mx-auto p-2">
 			<h1 className="text-center">To Do List</h1>
 			<input
 				className="w-100 mb-2 rounded"
